83810f102b22b162ba623061e729e9cc8e7b8871
andreyshilov0 <an.schilow@yandex.ru>
2024-01-19 15:05:06 +0300

    dont work

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,18 +1,14 @@
-module Users
-  class SessionsController < Devise::SessionsController
-    def create
-      self.resource = warden.authenticate!(auth_options)
-      set_flash_message!(:notice, :signed_in)
-      sign_in(resource_name, resource)
-      yield resource if block_given?
-      respond_with resource, location: after_sign_in_path_for(resource)
-    end
+class Users::SessionsController < Devise::SessionsController
+  respond_to :html, :turbo_stream
 
-    def destroy
-      signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-      set_flash_message! :notice, :signed_out if signed_out
-      yield if block_given?
-      respond_to_on_destroy
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    
+    respond_to do |format|
+      format.html { respond_with resource, location: after_sign_in_path_for(resource) }
+      format.turbo_stream { render turbo_stream: turbo_stream.replace('form_id', partial: 'devise/sessions/successful_login') }
     end
   end
 end
